cbc505366dc6f0362093b8c3cbff9d884c16f70a
TaylorLearnsToCode <wtbeeson@gmail.com>
2022-05-30 22:08:45 -0400

    added melee weapons

--- scripts/character-constants.mjs
+++ scripts/character-constants.mjs
@@ -1,2 +1,4 @@
 export const CHARACTER_CLASSES = ['Fighting Man', 'Magic User', 'Cleric'];
 export const ALIGNMENTS = ['Law', 'Chaos', 'Undeclared'];
+export const TROOP_TYPES = ['Armored', 'Heavy', 'Light'];
+export const COIN_ABBREVIATIONS = ['cp', 'sp', 'gp'];
